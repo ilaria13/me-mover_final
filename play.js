447641550992810171c6058c5abd3f64e52c17bd
Ilaria Cherubini <ilar0061@stud.kea.dk>
2021-03-23 10:35:02 +0100

    play button ok

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
-const play = document.querySelector(".play");
+const play = document.querySelector(".play_button");
 play.addEventListener("click", function () {
   document.querySelector("#videoBg").play();
+  document.querySelector(".play_button").classList.add("hidden");
 });
